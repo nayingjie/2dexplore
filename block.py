cf51f64d99092d3f1d22c870160be0543b2f0594
SopaXorzTaker <yellowmark@yandex.ru>
2014-10-05 13:54:01 +0400

    Rewrite liquid flow, fixed bugs

--- block.py
+++ block.py
@@ -1,4 +1,5 @@
 __author__ = 'mark'
+import pygame.image
 BLOCK_AIR = 0
 BLOCK_STONE = 1
 BLOCK_DIRT = 2
@@ -6,3 +7,18 @@ BLOCK_GRASS = 3
 BLOCK_DIAMOND = 4
 BLOCK_LAVA = 5
 BLOCK_WATER = 6
+BLOCK_LAVA_FLOWING = 7
+BLOCK_WATER_FLOWING = 8
+non_solid = [BLOCK_AIR, BLOCK_LAVA, BLOCK_WATER, BLOCK_WATER_FLOWING, BLOCK_LAVA_FLOWING]
+deadly = [BLOCK_LAVA, BLOCK_LAVA_FLOWING]
+textures = {
+    BLOCK_STONE: pygame.image.load('textures/stone.png'),
+    BLOCK_AIR: pygame.image.load('textures/air.png'),
+    BLOCK_DIRT: pygame.image.load('textures/dirt.png'),
+    BLOCK_GRASS: pygame.image.load('textures/grass.png'),
+    BLOCK_DIAMOND: pygame.image.load('textures/diamond_ore.png'),
+    BLOCK_LAVA: pygame.image.load('textures/lava.png'),
+    BLOCK_WATER: pygame.image.load('textures/water.png'),
+    BLOCK_LAVA_FLOWING: pygame.image.load('textures/lava.png'),
+    BLOCK_WATER_FLOWING: pygame.image.load('textures/water.png')
+}
--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import json
 import os.path
 import gzip
 from pygame.locals import *
+import random
 
 import generator
 import block
@@ -13,7 +14,7 @@ import block
 TILESIZE = 32
 MAP_X = 20
 MAP_Y = 20
-blocks = [block.BLOCK_STONE, block.BLOCK_DIRT, block.BLOCK_GRASS, block.BLOCK_DIAMOND,
+inventory_blocks = [block.BLOCK_STONE, block.BLOCK_DIRT, block.BLOCK_GRASS, block.BLOCK_DIAMOND,
           block.BLOCK_LAVA, block.BLOCK_WATER]
 start_time = 0
 player_pos = [5, 0]
@@ -31,12 +32,12 @@ if os.path.isfile("explore_save.gz"):
     except Exception as e:
         print "[DBG] Can't load saved world: %s" % str(e)
         world = generator.generate_world(MAP_X, MAP_Y)
-        inv = [0 for x in range(0, len(blocks))]
+        inv = [0 for x in range(0, len(inventory_blocks))]
     finally:
         save_file.close()
 else:
     world = generator.generate_world(MAP_X, MAP_Y)
-    inv = [0 for x in range(0, len(blocks))]
+    inv = [0 for x in range(0, len(inventory_blocks))]
 player_texture = pygame.image.load("textures/player.png")
 current_block = block.BLOCK_STONE
 falling = False
@@ -50,16 +51,7 @@ COLORS = {
     'grey': (127, 127, 127)
 }
 
-non_solid = [block.BLOCK_AIR, block.BLOCK_LAVA, block.BLOCK_WATER]
-textures = {
-    block.BLOCK_STONE: pygame.image.load('textures/stone.png'),
-    block.BLOCK_AIR: pygame.image.load('textures/air.png'),
-    block.BLOCK_DIRT: pygame.image.load('textures/dirt.png'),
-    block.BLOCK_GRASS: pygame.image.load('textures/grass.png'),
-    block.BLOCK_DIAMOND: pygame.image.load('textures/diamond_ore.png'),
-    block.BLOCK_LAVA: pygame.image.load('textures/lava.png'),
-    block.BLOCK_WATER: pygame.image.load('textures/water.png')
-}
+
 pygame.init()
 pygame.display.set_caption("2DExplore")
 font = pygame.font.SysFont("FreeSansBold", 18)  # Fonts should be inited after pygame.init()
@@ -84,7 +76,7 @@ def new_world():
     global world, inv, player_pos
     world = generator.generate_world(MAP_X, MAP_Y)
     # noinspection PyUnusedLocal
-    inv = [0 for i in range(0, len(blocks))]
+    inv = [0 for i in range(0, len(inventory_blocks))]
     player_pos = [5, 0]
 
 
@@ -92,56 +84,92 @@ def tick():
     global player_pos, falling, falldelay
     if god_mode:
         falling = False
-    lava_spread = 0
-    water_spread = 0
     if falling:
         falldelay += 1
 
-    elif player_pos[0] < MAP_X-1 and 0 < player_pos and world[player_pos[1]][player_pos[0]+1] in non_solid:
+    elif player_pos[0] < MAP_X-1 and 0 < player_pos and world[player_pos[1]][player_pos[0]+1] in block.non_solid:
         falling = True
     if falldelay == 3:
         player_pos[0] += 1
         falldelay = 0
         falling = False
+
     for x2 in range(MAP_X):
         for y2 in range(MAP_Y):
             if world[x2][y2] == block.BLOCK_LAVA:
                 if 0 < x2 < MAP_X - 1:
-                    lava_spread += 1
-                    if world[x2 - 1][y2] == block.BLOCK_AIR and lava_spread < 19:
-                        world[x2 - 1][y2] = block.BLOCK_LAVA
-                        lava_spread -= 8
-                    elif world[x2 + 1][y2] == block.BLOCK_AIR and lava_spread < 19:
-                        world[x2 + 1][y2] = block.BLOCK_LAVA
-                        lava_spread -= 8
+                    if world[x2 - 1][y2] == block.BLOCK_AIR:
+                        world[x2 - 1][y2] = block.BLOCK_LAVA_FLOWING
+                        break
+                    elif world[x2 + 1][y2] == block.BLOCK_AIR:
+                        world[x2 + 1][y2] = block.BLOCK_LAVA_FLOWING
+                        break
                     elif 0 < y2 < MAP_Y - 1 and world[x2][y2 + 1] == block.BLOCK_AIR:
                         world[x2][y2 + 1] = block.BLOCK_LAVA
-                        lava_spread = 0
+                        break
             elif world[x2][y2] == block.BLOCK_WATER:
                 if 0 < x2 < MAP_X - 1:
-                    water_spread += 1
-                    if world[x2 - 1][y2] == block.BLOCK_AIR and water_spread < 15:
-                        world[x2 - 1][y2] = block.BLOCK_WATER
-                        water_spread -= 8
-                    elif world[x2 + 1][y2] == block.BLOCK_AIR and water_spread < 15:
-                        world[x2 + 1][y2] = block.BLOCK_WATER
-                        water_spread -= 8
+                    if world[x2 - 1][y2] == block.BLOCK_AIR:
+                        world[x2 - 1][y2] = block.BLOCK_WATER_FLOWING
+                        break
+                    elif world[x2 + 1][y2] == block.BLOCK_AIR:
+                        world[x2 + 1][y2] = block.BLOCK_WATER_FLOWING
+                        break
                     elif 0 < y2 < MAP_Y - 1 and world[x2][y2 + 1] == block.BLOCK_AIR:
                         world[x2][y2 + 1] = block.BLOCK_WATER
-                        water_spread = 0
+                        break
+            #elif world[x2][y2] == block.BLOCK_WATER_FLOWING:
+            #    if 0 < x2 < MAP_X - 1:
+            #        if world[x2 - 1][y2] == block.BLOCK_AIR:
+            #            world[x2 - 1][y2] = block.BLOCK_WATER_FLOWING
+            #            break
+            #        elif world[x2 + 1][y2] == block.BLOCK_AIR:
+            #            world[x2 + 1][y2] = block.BLOCK_WATER_FLOWING
+            #            break
+            #elif world[x2][y2] == block.BLOCK_LAVA_FLOWING:
+            #    if 0 < x2 < MAP_X - 1:
+            #        if world[x2 - 1][y2] == block.BLOCK_AIR:
+            #            world[x2 - 1][y2] = block.BLOCK_LAVA_FLOWING
+            #            break
+            #        elif world[x2 + 1][y2] == block.BLOCK_AIR:
+            #            world[x2 + 1][y2] = block.BLOCK_LAVA_FLOWING
+            #            break
+
     #print "[DBG] Lava spread level: %d" % lava_spread
     #print "[DBG] Water spread level: %d" % water_spread
 
-def destroy_block(bx, by):
+def destroy_block(blk_x, blk_y, add_inventory=True):
     global world, inv
-    blk = world[bx][by]
-    if blk in blocks:
-        world[bx][by] = block.BLOCK_AIR
-        inv[blocks.index(blk)]+=1
+    blk = world[blk_x][blk_y]
+    if blk in inventory_blocks or not add_inventory:
+        world[blk_x][blk_y] = block.BLOCK_AIR
+        if add_inventory:
+            inv[inventory_blocks.index(blk)] += 1
+
+
+def explode(exp_x, exp_y, exp_radius, add_inventory=False):
+    for ex in range(exp_x - exp_radius, exp_x + exp_radius):
+        for ey in range(exp_y - exp_radius, exp_y + exp_radius):
+            if check_pos(ex, ey):
+                if (ey - exp_radius) > (exp_radius / 2):
+                    if random.randint(0, 8) < 8:
+                        destroy_block(ex, ey, add_inventory)
+                else:
+                    destroy_block(ex, ey, add_inventory)
+
+
+def check_pos(pos_x, pos_y):
+    if 0 <= pos_x < MAP_X:
+        if 0 <= pos_y < MAP_Y:
+            #print "[DBG] check_pos(%d, %d) == True" % (pos_x, pos_y)
+            return True
+    #print "[DBG] check_pos(%d, %d) == False" % (pos_x, pos_y)
+    return False
+
 clk = pygame.time.Clock()
 while True:
     clk.tick(20)
-    if pygame.time.get_ticks() - start_time >= 100:
+    if pygame.time.get_ticks() - start_time >= 50:
         tick()
         start_time = pygame.time.get_ticks()
     game_over = False
@@ -175,16 +203,16 @@ while True:
             keys = pygame.key.get_pressed()
             if event.key == K_w and player_pos[0] in range(1, MAP_X) and (not falling or god_mode):
                 player_pos[0] -= 1
-                if not world[player_pos[1]][player_pos[0]] in non_solid and not keys[K_LSHIFT] and not god_mode:
+                if not world[player_pos[1]][player_pos[0]] in block.non_solid and not keys[K_LSHIFT] and not god_mode:
                     player_pos = prev_pos
                 if keys[K_LSHIFT]:
                     bx, by = player_pos[1], player_pos[0]
                     destroy_block(bx, by)
                 else:
                     jumping = True
-            elif event.key == K_s and player_pos[0] in range(0, MAP_X - 1):
+            elif event.key == K_s and player_pos[0] in range(0, MAP_X - 2):
                 player_pos[0] += 1
-                if not world[player_pos[1]][player_pos[0]] in non_solid and not keys[K_LSHIFT]:
+                if not world[player_pos[1]][player_pos[0]] in block.non_solid and not keys[K_LSHIFT]:
                     player_pos = prev_pos
                 if keys[K_LSHIFT]:
                     bx, by = player_pos[1], player_pos[0]
@@ -193,16 +221,16 @@ while True:
                 falling = False
                 falldelay = 0
                 player_pos[1] -= 1
-                if not world[player_pos[1]][player_pos[0]] in non_solid and not keys[K_LSHIFT]:
+                if not world[player_pos[1]][player_pos[0]] in block.non_solid and not keys[K_LSHIFT]:
                     player_pos = prev_pos
                 if keys[K_LSHIFT]:
                     bx, by = player_pos[1], player_pos[0]
                     destroy_block(bx, by)
-            elif event.key == K_d and player_pos[1] in range(0, MAP_Y - 1):
+            elif event.key == K_d and player_pos[1] in range(0, MAP_Y - 2):
                 falling = False
                 falldelay = 0
                 player_pos[1] += 1
-                if not world[player_pos[1]][player_pos[0]] in non_solid and not keys[K_LSHIFT]:
+                if not world[player_pos[1]][player_pos[0]] in block.non_solid and not keys[K_LSHIFT]:
                     player_pos = prev_pos
                 if keys[K_LSHIFT]:
                     bx, by = player_pos[1], player_pos[0]
@@ -210,35 +238,38 @@ while True:
             elif event.key == K_z:
                 # print "Debug: placing block at %d %d, previous was %d" % (px, py, world[px][py])
                 if (inv[current_block] > 0 or god_mode) and block_under == block.BLOCK_AIR:
-                    world[px][py] = blocks[current_block]
+                    world[px][py] = inventory_blocks[current_block]
                     if not god_mode:
                         inv[current_block] -= 1
             elif event.key == K_x:
-                if world[px][py] in blocks:
-                    block_index = blocks.index(block_under)
+                if world[px][py] in inventory_blocks:
+                    block_index = inventory_blocks.index(block_under)
                     inv[block_index] += 1
                     world[px][py] = block.BLOCK_AIR
             elif event.key == K_1:
-                current_block = (current_block + 1) % len(blocks)
+                current_block = (current_block + 1) % len(inventory_blocks)
             elif event.key == K_ESCAPE:
                 new_world()
             elif event.key == K_F1:
                 god_mode = not god_mode
+            elif event.key == K_e:
+                explode(px, py, 2, True)
     for x in range(MAP_X):
         for y in range(MAP_Y):
-            DISPLAY.blit(textures[world[x][y]], (x * TILESIZE, y * TILESIZE))
+            DISPLAY.blit(block.textures[world[x][y]], (x * TILESIZE, y * TILESIZE))
     # gravity
     # newHeight = 0
     #while world[player_pos[0]][newHeight] == block.BLOCK_AIR:
     #    newHeight += 1
     #player_pos[0] = newHeight - 1
     DISPLAY.blit(player_texture, (player_pos[1] * TILESIZE, player_pos[0] * TILESIZE))
-    coordsLabel = font.render("Coords: %d, %d   %d fps" % (player_pos[0], player_pos[1], clk.get_fps())
-                              , True, COLORS['white'], COLORS['black'])
+    debugText = "Coords: %d, %d   %d fps" % (player_pos[0], player_pos[1], clk.get_fps()) + \
+                (" GOD MODE" if god_mode else "")
+    debugLabel = font.render(debugText, True, COLORS['white'], COLORS['black'])
     inventoryLabel = font.render(" x %d" % (inv[current_block]), True, COLORS['white'],
                                  COLORS['black'])
-    DISPLAY.blit(coordsLabel, (0, 0))
+    DISPLAY.blit(debugLabel, (0, 0))
     DISPLAY.fill(0, (0, MAP_X * TILESIZE, MAP_Y * TILESIZE, 37))
-    DISPLAY.blit(textures[blocks[current_block]], (0, MAP_Y * TILESIZE + 5))
+    DISPLAY.blit(block.textures[inventory_blocks[current_block]], (0, MAP_Y * TILESIZE + 5))
     DISPLAY.blit(inventoryLabel, (32, MAP_Y * TILESIZE + 5))
     pygame.display.update()
