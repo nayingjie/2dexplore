02d2ed0e341635d3e9cc34efcdd54af0a1db1d35
SopaXorzTaker <yellowmark@yandex.ru>
2015-07-27 17:42:17 +0300

    All PNG's are RGBA.

--- block.py
+++ block.py
@@ -47,13 +47,13 @@ def load_textures(append_path=""):
     BLOCK_TEXTURES = {
         BLOCK_STONE: pygame.image.fromstring(stone.tobytes(), (32, 32), "RGBA"),
         BLOCK_AIR: pygame.image.fromstring(air.tobytes(), (32, 32), "RGBA"),
-        BLOCK_DIRT: pygame.image.fromstring(dirt.tobytes(), (32, 32), "RGB"),
-        BLOCK_GRASS: pygame.image.fromstring(grass.tobytes(), (32, 32), "RGB"),
-        BLOCK_DIAMOND: pygame.image.fromstring(diamond_ore.tobytes(), (32, 32), "RGB"),
-        BLOCK_LAVA: pygame.image.fromstring(lava.tobytes(), (32, 32), "RGB"),
+        BLOCK_DIRT: pygame.image.fromstring(dirt.tobytes(), (32, 32), "RGBA"),
+        BLOCK_GRASS: pygame.image.fromstring(grass.tobytes(), (32, 32), "RGBA"),
+        BLOCK_DIAMOND: pygame.image.fromstring(diamond_ore.tobytes(), (32, 32), "RGBA"),
+        BLOCK_LAVA: pygame.image.fromstring(lava.tobytes(), (32, 32), "RGBA"),
         BLOCK_WATER: pygame.image.fromstring(water.tobytes(), (32, 32), "RGBA"),
-        BLOCK_LAVA_FLOWING: pygame.image.fromstring(lava.tobytes(), (32, 32), "RGB"),
+        BLOCK_LAVA_FLOWING: pygame.image.fromstring(lava.tobytes(), (32, 32), "RGBA"),
         BLOCK_WATER_FLOWING: pygame.image.fromstring(water.tobytes(), (32, 32), "RGBA"),
-        BLOCK_LEAVES: pygame.image.fromstring(leaves.tobytes(), (32, 32), "RGB"),
-        BLOCK_LOG: pygame.image.fromstring(log.tobytes(), (32, 32), "RGB")
+        BLOCK_LEAVES: pygame.image.fromstring(leaves.tobytes(), (32, 32), "RGBA"),
+        BLOCK_LOG: pygame.image.fromstring(log.tobytes(), (32, 32), "RGBA")
     }
